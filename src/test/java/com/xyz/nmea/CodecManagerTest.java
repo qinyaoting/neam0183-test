dae7ca767c14446e649a680dd1e5857e70c7ddf5
qyt <qyt@jusfoun.com>
2016-08-29 14:41:41 +0800

    use mockito to verify decoded object replace assertThat+equal to with assertEquals

--- src/test/java/com/xyz/nmea/CodecManagerTest.java
+++ src/test/java/com/xyz/nmea/CodecManagerTest.java
@@ -2,23 +2,27 @@ package com.xyz.nmea;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by wuf2 on 2/21/2015.
  */
 public class CodecManagerTest {
 
-
     @Test
-    public void decodeMessage() throws Exception {
-        String content = "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76\n";
+    public void decodeRmcMessage() throws Exception {
+        String content = "$GPRMC,092751.000,A,5321.6802,N,00630.3371,W,0.06,31.66,280511,,,A*45\r\n";
         CodecManager manager = new CodecManager();
         manager.addObserver(new Observer() {
             @Override
@@ -26,8 +30,13 @@ public class CodecManagerTest {
                 System.out.println(arg);
             }
         });
+
+        Observer mockObserver = mock(Observer.class);
+        manager.addObserver(mockObserver);
+
         manager.decode(content);
 
+        verify(mockObserver, times(1)).update(eq(manager), any());
     }
 
     @Test
@@ -39,10 +48,40 @@ public class CodecManagerTest {
                 System.out.println(arg);
             }
         });
+
+        Observer mockObserver = mock(Observer.class);
+        manager.addObserver(mockObserver);
+
         manager.decode("$GPRMC,092751.000,A,5");
         manager.decode("321.6802,N,00630.3371,W,0.06,31.66,280511,,,A*45\r\n");
+
+        verify(mockObserver, times(1)).update(eq(manager), any());
     }
 
+    @Test
+    public void decodeMultipleMessage() throws Exception {
+        CodecManager manager = new CodecManager();
+        Observer mockObserver = mock(Observer.class);
+        manager.addObserver(mockObserver);
+
+        String content = "$GPRMC,092751.000,A,5321.6802,N,00630.3371,W,0.06,31.66,280511,,,A*45\r\n$GPRMC,092751.000,A,5321.6802,N,00630.3371,W,0.06,31.66,280511,,,A*45\r\n";
+        manager.decode(content);
+
+        content = "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76\r\n"
+                + "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A\r\n"
+                + "$GPRMC,092751.000,A,5321.6802,N,00630.3371,W,0.06,31.66,280511,,,A*45\r\n"
+                + "!AIVDM,1,1,,B,16:>>s5Oh08dLO8AsMAVqptj0@>p,0*67\r\n";
+        manager.decode(content);
+
+        verify(mockObserver, times(6)).update(eq(manager), any());
+    }
+
+    @Test(expected = ClassNotFoundException.class)
+    public void decodeInvalidMessage() throws Exception {
+        String content = "$GPAAM,A,A,0.10,N,WPTNME*32\r\n$GPRMC,092751.000,A,5321.6802,N,00630.3371,W,0.06,31.66,280511,,,A*45\r\n";
+        CodecManager manager = new CodecManager();
+        manager.decode(content);
+    }
 
     @Test
     public void encode() {
@@ -55,7 +94,7 @@ public class CodecManagerTest {
                     CodecManager manager = new CodecManager();
                     AbstractNmeaObject obj = (AbstractNmeaObject) arg;
                     List<String> contents = manager.encode(obj);
-                    assertThat(contents.get(0), equalTo(content));
+                    assertEquals(Arrays.asList(content), contents);
                 } catch (Exception e) {
                     assertTrue("Expect no exception " + e, false);
                 }
--- src/test/java/com/xyz/nmea/GgaNmeaCodecTest.java
+++ src/test/java/com/xyz/nmea/GgaNmeaCodecTest.java
@@ -2,12 +2,19 @@ package com.xyz.nmea;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by chin on 8/22/16.
@@ -23,6 +30,8 @@ public class GgaNmeaCodecTest {
                 System.out.println(arg);
             }
         });
+        Observer mockObserver = mock(Observer.class);
+        codec.addObserver(mockObserver);
         String content = "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76\r\n";
         codec.decode(content);
 
@@ -31,7 +40,8 @@ public class GgaNmeaCodecTest {
 
         content = "$GPGGA,092204.999,4250.5589,S,14718.5084,E,1,04,24.4,19.7,M,,,,0000*1F\r\n";
         codec.decode(content);
-        codec.deleteObservers();
+        //codec.deleteObservers();
+        verify(mockObserver, times(3)).update(eq(codec), any());
 
     }
 
@@ -45,7 +55,8 @@ public class GgaNmeaCodecTest {
             public void update(Observable o, Object arg) {
                 AbstractNmeaObject obj = (AbstractNmeaObject) arg;
                 List<String> contents = codec.encode(obj);
-                assertThat(contents.get(0), equalTo(content));
+                //assertThat(contents.get(0), equalTo(content));
+                assertEquals(Arrays.asList(content), contents);
             }
         });
         codec.decode(content);
